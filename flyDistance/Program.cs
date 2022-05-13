a39667f52ddce0d6a9f0e9d6c788151ff2bc466a
Sinziana Sovejanu <sv.sinziana@gmail.com>
2022-05-13 23:37:23 +0300

    first commit

---
+++ flyDistance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace flyDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Z, X, t1, t2, t, d;
+            string inputData = Console.ReadLine();
+            Z = Convert.ToInt32(inputData);
+            inputData = Console.ReadLine();
+            X = Convert.ToInt32(inputData);
+            t1 = Z / (4 * X);
+            t2 = Z / (X * 2);
+            t = t2 - t1;
+            d = 2 * X * t;
+
+            Console.Write(d);
+            
+        }
+    }
+}
+
